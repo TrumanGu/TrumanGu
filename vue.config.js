1b81f55e4981f2e687a48700a5ef768d65ed0b2f
TrumanGu <1227085585@qq.com>
2018-11-04 23:24:11 +0800

    'fix'

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  assetsDir: './'
+  assetsDir: './',
+  baseUrl: '',
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
