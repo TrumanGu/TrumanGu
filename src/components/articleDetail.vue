d6022d65012904205de092148c03a24f0068ebfa
TrumanGu <1227085585@qq.com>
2019-06-14 01:10:20 +0800

    feat: 加入颜色渐变函数

--- src/components/articleDetail.vue
+++ src/components/articleDetail.vue
@@ -1,17 +1,25 @@
 <template>
-  <section class="tg-center article-detail">
-    <div class="center-wrapper">
+  <section class="tg-center">
+    <div class="center-wrapper article-detail">
       <article-item
+        v-if="!detailLoading"
         :createdAt="createdAt"
         :body="content"
         :id="Number.parseInt(id)"
         :fullTime="readTime"
       ></article-item>
+      <section v-else class="loading-secition">
+        <div class="dot-loading"></div>
+        <div class="dot-loading"></div>
+        <div class="dot-loading"></div>
+        <div class="dot-loading"></div>
+        <div class="dot-loading"></div>
+      </section>
       <div class="comments">
-        <vue-disqus shortname="trumangu-fun" :identifier="id+''"></vue-disqus>
         <!-- url="http://example.com/path" -->
       </div>
     </div>
+    <vue-disqus shortname="trumangu-fun" :identifier="id+''"></vue-disqus>
   </section>
 </template>
 
@@ -26,16 +34,21 @@ export default {
       content: "",
       createdAt: "",
       id: "",
-      readTime: 0
+      readTime: 0,
+      detailLoading: true // 文章详情页加载动画
     };
   },
   created() {
     this.id = this.$route.params.id;
-    queryArticle(this.id).then(({ result }) => {
-      this.createdAt = result.createdAt;
-      this.content = result.content;
-      this.readTime = Number.parseInt(this.content.length / 200);
-    });
+    queryArticle(this.id)
+      .then(({ result }) => {
+        this.createdAt = result.createdAt;
+        this.content = result.content;
+        this.readTime = Number.parseInt(this.content.length / 200);
+      })
+      .finally(_ => {
+        this.detailLoading = false;
+      });
   },
   mounted() {}
 };
@@ -43,5 +56,40 @@ export default {
 
 <style lang="scss">
 .article-detail {
+  min-height: 300px;
+  position: relative;
+  @keyframes slide {
+    0% {
+      transform: scale(1);
+    }
+
+    50% {
+      opacity: 0.3;
+      transform: scale(2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  .loading-secition {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .dot-loading {
+      width: 24px;
+      height: 24px;
+      background: #3ac;
+      border-radius: 100%;
+      display: inline-block;
+      animation: slide 1s infinite;
+      @for $i from 1 through 5 {
+        &:nth-child(#{$i}) {
+          animation-delay: calc(0.1s * #{$i});
+          background: rgb(51 * $i, 170, 204);
+        }
+      }
+    }
+  }
 }
 </style>
